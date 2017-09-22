9cb37736644c546e2d5de4b1bbe6cf517ea83af0
folknor <folk@folk.wtf>
2017-09-22 20:18:14 +0200

    Bump to 1.8.0.

--- modinfo.lua
+++ modinfo.lua
@@ -4,7 +4,7 @@ return {
 	name = "Skittish Scientists",
 	tags = { "Fixes", "Gameplay" },
 	picture = "thumb.png",
-	supported_version = "1.6.*",
+	supported_version = "1.8.*",
 	remote_file_id = 785582857,
 	readme = "readme.md",
 	steambb = "steam.bbcode",
